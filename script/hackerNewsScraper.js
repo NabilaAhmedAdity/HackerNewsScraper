e9fce09a7c605b5cf8d5354921d3cb9bc2b4656c
Nabila Ahmed <nabilaahmed.adity@gmail.com>
2019-07-16 16:16:05 +0100

    Add functions to get top valid posts

--- script/hackerNewsScraper.js
+++ script/hackerNewsScraper.js
@@ -1,19 +1,33 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const validUrl = require('valid-url');
 
 class HackerNewsScraper {
   constructor(n) {
     let numberOfPosts = Number(n);
 
-    if(!this._validateProp(numberOfPosts)) {
+    if(!this._isValidateProp(numberOfPosts)) {
       throw 'Number of posts should be a positive integer <= 100';
     }
     this.numberOfPosts = numberOfPosts;
     this.URL = 'https://news.ycombinator.com';
+    this.topPosts = [];
   }
 
-  _validateProp(numberOfPosts) {
-    return !numberOfPosts || !Number.isInteger(numberOfPosts) || numberOfPosts<=0 || numberOfPosts>100 ? false : true;
+  _isPositiveInteger(value) {
+    return !Number.isInteger(value) || value<=0 ? false : true;
+  }
+
+  _isNonNegativeInteger(value) {
+    return !Number.isInteger(value) || value<0 ? false : true;
+  }
+
+  _isValidString(value) {
+    return !value || value.length <= 0 || value.length > 256 ? false : true;
+  }
+
+  _isValidateProp(numberOfPosts) {
+    return !this._isPositiveInteger(numberOfPosts) || numberOfPosts>100 ? false : true;
   }
 
   async _getNumberOfPostsPerPage() {
@@ -26,14 +40,89 @@ class HackerNewsScraper {
     }
 
     try {
-      let html = cheerio.load(response.data);
-      let postPerPage = html('.athing').length;
+      let $ = cheerio.load(response.data);
+      let postPerPage = $('.athing').length;
       return postPerPage;
     } catch(err) {
       throw err;
     }
   }
 
+  _isValidPost(post) {
+    return !this._isValidString(post.title) || !this._isValidString(post.author) ||
+            !validUrl.isUri(post.uri) || !this._isNonNegativeInteger(post.rank) ||
+            !this._isNonNegativeInteger(post.points) || !this._isNonNegativeInteger(post.comments) ? false : true;
+  }
+
+  // This function takes a HTML page, parse the posts one by one until it reaches the limit(numberOfPosts).
+  // Then check if the post is valid or not
+  // If a post is valid, add it to the topPosts list
+  _parseHtml(html) {
+    try {
+      let $ = cheerio.load(html);
+      let titleRows = $('.athing');
+
+      if(!titleRows.length) {
+        throw `Not enough valid posts. Total valid posts found ${this.topPosts.length}, required ${this.numberOfPosts}`;
+      }
+
+      for(let i=0; i<titleRows.length; i++) {
+        if(this.topPosts.length === this.numberOfPosts) {
+          break;
+        }
+
+        let titleRow = titleRows[i];
+        let subtextRow = $(titleRow).next();
+        // First row contains title, uri and rank
+        // Second row contains points, author and comments
+        let post = {
+          title: $(titleRow).find('.storylink').text(),
+          uri: $(titleRow).find('.storylink').attr('href'),
+          author: $(subtextRow).find('.hnuser').text(),
+          rank: Number($(titleRow).find('.rank').text().slice(0, -1)),
+          points: Number($(subtextRow).find('.score').text().split(' ')[0]),
+          comments: Number($(subtextRow).find('.subtext').children().last().text().slice(0, -8)),
+        }
+
+        if(this._isValidPost(post)) {
+          this.topPosts.push(post);
+        }
+      }
+    }
+    catch(err) {
+      throw err;
+    }
+  }
+
+  // This function hits the urls and return the promises in list
+  _getAPIResponses(startPage, endPage) {
+    let responses = [];
+    
+    for(let page=startPage; page<=endPage; page++) {
+      responses.push(axios.get(`${this.URL}/news?p=${page}`));
+    }
+    return responses;
+  }
+
+  async _getTopPosts() {
+    let offset = 0;
+
+    while(this.topPosts.length < this.numberOfPosts) {
+      let startPage = offset + 1;
+      let endPage = offset + Math.ceil((this.numberOfPosts-this.topPosts.length) / this.postsPerPage);
+      offset = endPage;
+      let responses = this._getAPIResponses(startPage, endPage);
+
+      try {
+        let htmls = (await Promise.all(responses)).map(({data}) => data);
+        htmls.forEach(html => this._parseHtml(html))
+      } catch(err) {
+        throw err;
+      }
+    }
+  }
+
+  // This function returns the top N valid posts from Hacker News, where N is set during initiazing the instance
   async getTopPosts() {
     try {
       this.postsPerPage = await this._getNumberOfPostsPerPage();
@@ -45,8 +134,12 @@ class HackerNewsScraper {
       throw `No posts found in HackerNews with url: ${this.URL}`;
     }
 
-    let topPosts = [];
-    return topPosts;
+    try {
+      await this._getTopPosts();
+      return this.topPosts;
+    } catch(err) {
+      throw err;
+    }
   }
 }
 
--- test/hackerNewsScraper-Spec.js
+++ test/hackerNewsScraper-Spec.js
@@ -45,6 +45,11 @@ describe('Test for HackerNewsScraper initialization', () => {
     let scraper = new HackerNewsScraper(50);
     expect(scraper.URL).to.eql('https://news.ycombinator.com');
   });
+
+  it('should initialize with empty posts list', () => {
+    let scraper = new HackerNewsScraper(50);
+    expect(scraper.topPosts.length).to.eql(0);
+  });
 });
 
 describe('Test for API call', () => {
@@ -88,5 +93,228 @@ describe('Test for API call', () => {
       .reply(200, dummyResponse);
     await scraper.getTopPosts();
     expect(scraper.postsPerPage).to.eql(1);
+  });
+});
+
+describe('Test for posts validity', () => {
+  let scraper;
+
+  beforeEach(() => {
+    const dummyResponse1 = '<html> <body> <table id="hnmain"> <tbody> <tr class="athing"> <td class="title"><span class="rank">1.</span></td> <td class="title"><a href="https://www.bbc.co.uk/news/post-1" class="storylink">Post-1</a></td> </tr> <tr> <td class="subtext"> <span class="score">10 points</span> by <a href="user?id=hanoz" class="hnuser">Author-1</a> <span class="age"><a href="item?id=20439425">27 minutes ago</a></span> <span id="unv_20439425"></span> | <a href="hide?id=20439425&goto=news">hide</a> | <a href="item?id=20439425">5 comment</a> </td> </tr> <tr class="spacer" style="height:5px"></tr> </tbody> </table> </body> </html>';
+    const dummyResponse3 = '<html> <body> <table id="hnmain"> <tbody> <tr class="athing"> <td class="title"><span class="rank">3.</span></td> <td class="title"><a href="https://www.bbc.co.uk/news/post-3" class="storylink">Post-3</a></td> </tr> <tr> <td class="subtext"> <span class="score">30 points</span> by <a href="user?id=hanoz" class="hnuser">Author-3</a> <span class="age"><a href="item?id=20439425">27 minutes ago</a></span> <span id="unv_20439425"></span> | <a href="hide?id=20439425&goto=news">hide</a> | <a href="item?id=20439425">15 comment</a> </td> </tr> <tr class="spacer" style="height:5px"></tr> </tbody> </table> </body> </html>';
+    scraper = new HackerNewsScraper(2);
+
+    nock(scraper.URL)
+      .get('/')
+      .reply(200, dummyResponse1);
+    nock(scraper.URL)
+      .get('/news?p=1')
+      .reply(200, dummyResponse1);
+    nock(scraper.URL)
+      .get('/news?p=3')
+      .reply(200, dummyResponse3);
+  });
+
+  it('should get the top two posts corectly', async () => {
+    const dummyResponse2 = '<html> <body> <table id="hnmain"> <tbody> <tr class="athing"> <td class="title"><span class="rank">2.</span></td> <td class="title"><a href="https://www.bbc.co.uk/news/post-2" class="storylink">Post-2</a></td> </tr> <tr> <td class="subtext"> <span class="score">20 points</span> by <a href="user?id=hanoz" class="hnuser">Author-2</a> <span class="age"><a href="item?id=20439425">27 minutes ago</a></span> <span id="unv_20439425"></span> | <a href="hide?id=20439425&goto=news">hide</a> | <a href="item?id=20439425">10 comment</a> </td> </tr> <tr class="spacer" style="height:5px"></tr> </tbody> </table> </body> </html>';
+    nock(scraper.URL)
+      .get('/news?p=2')
+      .reply(200, dummyResponse2);
+
+    let posts = await scraper.getTopPosts();
+    expect(posts).to.eql([
+      {
+        title: 'Post-1',
+        uri: 'https://www.bbc.co.uk/news/post-1',
+        author: 'Author-1',
+        rank: 1,
+        points: 10,
+        comments: 5
+      },
+      {
+        title: 'Post-2',
+        uri: 'https://www.bbc.co.uk/news/post-2',
+        author: 'Author-2',
+        rank: 2,
+        points: 20,
+        comments: 10
+      }
+    ]);
+  });
+
+  it('should ignore any post with empty title', async () => {
+    let dummyResponse2 = '<html> <body> <table id="hnmain"> <tbody> <tr class="athing"> <td class="title"><span class="rank">2.</span></td> <td class="title"><a href="https://www.bbc.co.uk/news/post-2" class="storylink"></a></td> </tr> <tr> <td class="subtext"> <span class="score">20 points</span> by <a href="user?id=hanoz" class="hnuser">Author-2</a> <span class="age"><a href="item?id=20439425">27 minutes ago</a></span> <span id="unv_20439425"></span> | <a href="hide?id=20439425&goto=news">hide</a> | <a href="item?id=20439425">10 comment</a> </td> </tr> <tr class="spacer" style="height:5px"></tr> </tbody> </table> </body> </html>';
+    nock(scraper.URL)
+      .get('/news?p=2')
+      .reply(200, dummyResponse2);
+
+    let posts = await scraper.getTopPosts();
+    expect(posts).to.eql([
+      {
+        title: 'Post-1',
+        uri: 'https://www.bbc.co.uk/news/post-1',
+        author: 'Author-1',
+        rank: 1,
+        points: 10,
+        comments: 5
+      },
+      {
+        title: 'Post-3',
+        uri: 'https://www.bbc.co.uk/news/post-3',
+        author: 'Author-3',
+        rank: 3,
+        points: 30,
+        comments: 15
+      }
+    ]);
+  });
+
+  it('should ignore any post with no author', async () => {
+    const dummyResponse2 = '<html> <body> <table id="hnmain"> <tbody> <tr class="athing"> <td class="title"><span class="rank">2.</span></td> <td class="title"><a href="https://www.bbc.co.uk/news/post-2" class="storylink">Post-2</a></td> </tr> <tr> <td class="subtext"> <span class="score">20 points</span> by <span class="age"><a href="item?id=20439425">27 minutes ago</a></span> <span id="unv_20439425"></span> | <a href="hide?id=20439425&goto=news">hide</a> | <a href="item?id=20439425">10 comment</a> </td> </tr> <tr class="spacer" style="height:5px"></tr> </tbody> </table> </body> </html>';
+    nock(scraper.URL)
+      .get('/news?p=2')
+      .reply(200, dummyResponse2);
+
+    let posts = await scraper.getTopPosts();
+    expect(posts).to.eql([
+      {
+        title: 'Post-1',
+        uri: 'https://www.bbc.co.uk/news/post-1',
+        author: 'Author-1',
+        rank: 1,
+        points: 10,
+        comments: 5
+      },
+      {
+        title: 'Post-3',
+        uri: 'https://www.bbc.co.uk/news/post-3',
+        author: 'Author-3',
+        rank: 3,
+        points: 30,
+        comments: 15
+      }
+    ]);
+  });
+
+  it('should ignore any post with invalid uri', async () => {
+    const dummyResponse2 = '<html> <body> <table id="hnmain"> <tbody> <tr class="athing"> <td class="title"><span class="rank">2.</span></td> <td class="title"><a href="Looks like an url" class="storylink">Post-2</a></td> </tr> <tr> <td class="subtext"> <span class="score">20 points</span> by <a href="user?id=hanoz" class="hnuser">Author-2</a> <span class="age"><a href="item?id=20439425">27 minutes ago</a></span> <span id="unv_20439425"></span> | <a href="hide?id=20439425&goto=news">hide</a> | <a href="item?id=20439425">10 comment</a> </td> </tr> <tr class="spacer" style="height:5px"></tr> </tbody> </table> </body> </html>';
+    nock(scraper.URL)
+      .get('/news?p=2')
+      .reply(200, dummyResponse2);
+
+    let posts = await scraper.getTopPosts();
+    expect(posts).to.eql([
+      {
+        title: 'Post-1',
+        uri: 'https://www.bbc.co.uk/news/post-1',
+        author: 'Author-1',
+        rank: 1,
+        points: 10,
+        comments: 5
+      },
+      {
+        title: 'Post-3',
+        uri: 'https://www.bbc.co.uk/news/post-3',
+        author: 'Author-3',
+        rank: 3,
+        points: 30,
+        comments: 15
+      }
+    ]);
+  });
+
+  it('should ignore any post with floating points', async () => {
+    const dummyResponse2 = '<html> <body> <table id="hnmain"> <tbody> <tr class="athing"> <td class="title"><span class="rank">2.</span></td> <td class="title"><a href="https://www.bbc.co.uk/news/post-2" class="storylink">Post-2</a></td> </tr> <tr> <td class="subtext"> <span class="score">20.7 points</span> by <a href="user?id=hanoz" class="hnuser">Author-2</a> <span class="age"><a href="item?id=20439425">27 minutes ago</a></span> <span id="unv_20439425"></span> | <a href="hide?id=20439425&goto=news">hide</a> | <a href="item?id=20439425">10 comment</a> </td> </tr> <tr class="spacer" style="height:5px"></tr> </tbody> </table> </body> </html>';
+    nock(scraper.URL)
+      .get('/news?p=2')
+      .reply(200, dummyResponse2);
+
+    let posts = await scraper.getTopPosts();
+    expect(posts).to.eql([
+      {
+        title: 'Post-1',
+        uri: 'https://www.bbc.co.uk/news/post-1',
+        author: 'Author-1',
+        rank: 1,
+        points: 10,
+        comments: 5
+      },
+      {
+        title: 'Post-3',
+        uri: 'https://www.bbc.co.uk/news/post-3',
+        author: 'Author-3',
+        rank: 3,
+        points: 30,
+        comments: 15
+      }
+    ]);
+  });
+
+  it('should ignore any post with negative rank', async () => {
+    const dummyResponse2 = '<html> <body> <table id="hnmain"> <tbody> <tr class="athing"> <td class="title"><span class="rank">-2.</span></td> <td class="title"><a href="https://www.bbc.co.uk/news/post-2" class="storylink">Post-2</a></td> </tr> <tr> <td class="subtext"> <span class="score">20 points</span> by <a href="user?id=hanoz" class="hnuser">Author-2</a> <span class="age"><a href="item?id=20439425">27 minutes ago</a></span> <span id="unv_20439425"></span> | <a href="hide?id=20439425&goto=news">hide</a> | <a href="item?id=20439425">10 comment</a> </td> </tr> <tr class="spacer" style="height:5px"></tr> </tbody> </table> </body> </html>';
+    nock(scraper.URL)
+      .get('/news?p=2')
+      .reply(200, dummyResponse2);
+
+    let posts = await scraper.getTopPosts();
+    expect(posts).to.eql([
+      {
+        title: 'Post-1',
+        uri: 'https://www.bbc.co.uk/news/post-1',
+        author: 'Author-1',
+        rank: 1,
+        points: 10,
+        comments: 5
+      },
+      {
+        title: 'Post-3',
+        uri: 'https://www.bbc.co.uk/news/post-3',
+        author: 'Author-3',
+        rank: 3,
+        points: 30,
+        comments: 15
+      }
+    ]);
+  });
+
+  it('should ignore any post with invalid integer comment', async () => {
+    const dummyResponse2 = '<html> <body> <table id="hnmain"> <tbody> <tr class="athing"> <td class="title"><span class="rank">2.</span></td> <td class="title"><a href="https://www.bbc.co.uk/news/post-2" class="storylink">Post-2</a></td> </tr> <tr> <td class="subtext"> <span class="score">20 points</span> by <a href="user?id=hanoz" class="hnuser">Author-2</a> <span class="age"><a href="item?id=20439425">27 minutes ago</a></span> <span id="unv_20439425"></span> | <a href="hide?id=20439425&goto=news">hide</a> | <a href="item?id=20439425">10c comment</a> </td> </tr> <tr class="spacer" style="height:5px"></tr> </tbody> </table> </body> </html>';
+    nock(scraper.URL)
+      .get('/news?p=2')
+      .reply(200, dummyResponse2);
+
+    let posts = await scraper.getTopPosts();
+    expect(posts).to.eql([
+      {
+        title: 'Post-1',
+        uri: 'https://www.bbc.co.uk/news/post-1',
+        author: 'Author-1',
+        rank: 1,
+        points: 10,
+        comments: 5
+      },
+      {
+        title: 'Post-3',
+        uri: 'https://www.bbc.co.uk/news/post-3',
+        author: 'Author-3',
+        rank: 3,
+        points: 30,
+        comments: 15
+      }
+    ]);
+  });
+
+  it('should throw an error if number of valid posts in not enough', async () => {
+    const dummyResponse2 = '<html> <body> <table id="hnmain"> <tbody> <tr class="athing"> <td class="title"><span class="rank">2.</span></td> <td class="title"><a href="https://www.bbc.co.uk/news/post-2" class="storylink">Post-2</a></td> </tr> <tr> <td class="subtext"> <span class="score">20 points</span> by <a href="user?id=hanoz" class="hnuser">Author-2</a> <span class="age"><a href="item?id=20439425">27 minutes ago</a></span> <span id="unv_20439425"></span> | <a href="hide?id=20439425&goto=news">hide</a> | <a href="item?id=20439425">10c comment</a> </td> </tr> <tr class="spacer" style="height:5px"></tr> </tbody> </table> </body> </html>';
+    const dummyResponse4 = '<html></html>'
+    nock(scraper.URL)
+      .get('/news?p=2')
+      .reply(200, dummyResponse2);
+    nock(scraper.URL)
+      .get('/news?p=4')
+      .reply(200, dummyResponse4);
+
+    scraper.numberOfPosts = 3;
+    await expect(scraper.getTopPosts()).to.be.rejectedWith('Not enough valid posts. Total valid posts found 2, required 3');
   });
 });
