03f670615fdf04a2127f1cb195f58f16d5c48bff
Nabila Ahmed <nabilaahmed.adity@gmail.com>
2019-07-17 12:27:46 +0100

    Convert the project into command line tool

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+#!/usr/bin/env node
 const { HackerNewsScraper } = require('hackerNewsScraper');
+const program = require('commander');
+const chalk = require('chalk');
 
 async function main() {
+  program
+  .version('0.1.0', '-v, --version')
+  .option('-p, --posts <posts>', 'Number of valid posts to extract')
+  .parse(process.argv);
+  
+  if(!program.posts) {
+    console.log(chalk.green('Should run with -p flag. Example: hackernews -p 10'));
+    return;
+  }
+
   try{
     let scraper = new HackerNewsScraper();
-    let posts = await scraper.getTopPosts(100);
+    let posts = await scraper.getTopPosts(program.posts);
     console.log(posts);
   } catch(err) {
-    console.log(err);
+    console.log(chalk.red(err));
   }
 }
 
